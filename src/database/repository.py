abc39b4efa6d74b67418d9427dc10208c5300f3d
Kirill Karaganov <kmkaraganov@edu.hse.ru>
2025-12-22 11:16:24 +0300

    Слил свою ветку с Арсом и сделал шифрование

--- src/database/repository.py
+++ src/database/repository.py
@@ -1,14 +1,12 @@
-from sqlalchemy import create_engine, Column, String, BigInteger, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.orm import declarative_base
 
-engine = create_engine("sqlite:///yandex_music_bot.db", echo=True)
+engine = create_engine("sqlite:///yandex_music_bot.db", echo=False)
 Base = declarative_base()
 
 class UserToken(Base):
     __tablename__ = "user_tokens"
-
     nickname = Column(String, primary_key=True)
     token = Column(String, nullable=False)
-
 
 Base.metadata.create_all(engine)
---
+++ src/database/yandex-music-bot-read.hcl
@@ -0,0 +1,3 @@
+path "secret/data/yandex-music-bot" {
+  capabilities = ["read"]
+}
